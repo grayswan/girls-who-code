120aef795f209116397e2ab8abb7c0beb3ec9b45
grayswan <swan.kristen@gmail.com>
2018-12-03 16:32:59 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,117 @@
+const changeLook = () => {
+  document.getElementById("button").classList.add("purple-button")
+  document.getElementById("body").classList.add("teal-background")
+  document.getElementById("button-text").classList.add("button-text-hide")
+  document.getElementById("sub-title").classList.replace("sub-title-hide", "sub-title-show")
+  document.getElementById("unicorn").classList.replace("unicorn-hide", "unicorn-show")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const changeLook = () => {
+//   document.getElementById("button").classList.add("pinkButton")
+//   document.getElementById("sub-title").classList.replace("sub-title-hide", "sub-title-show")
+//   document.getElementById("body").classList.add("background")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
